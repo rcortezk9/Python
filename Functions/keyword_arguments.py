428a4259a2f0aefa02e92a6ea7e307b6bf502934
Rene Cortez <rcortezk9@gmail.com>
2019-01-14 20:29:32 -0600

    Using Keyword arguments

---
+++ Functions/keyword_arguments.py
@@ -0,0 +1,10 @@
+# Keyword Arguments
+
+# create a function
+def book_description(book_type, author):
+    """display book information"""
+
+    print("\nThis is a " + book_type + "book.")
+    print("The author is " + author + ".")
+
+book_description(book_type='non-fiction', author='brad stone')
