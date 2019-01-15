6250e9962a9f337771a50aad8140e6c509c916f9
Rene Cortez <rcortezk9@gmail.com>
2019-01-14 19:28:54 -0600

    Use a while loop to remove an item out of a list.

---
+++ While loops/remove_from_list.py
@@ -0,0 +1,7 @@
+# Removing values from a list
+books = ['big data', 'checklist', 'the firm', 'tesla', 'the firm', 'the firm']
+
+while 'the firm' in books:
+    books.remove('the firm')
+
+print (books)
