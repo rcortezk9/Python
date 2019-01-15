1853a94ea8292755e3f8966ba90ff1761e304109
Rene Cortez <rcortezk9@gmail.com>
2019-01-14 20:21:21 -0600

    Positional argument

--- Functions/passing_into_to_functions.py
+++ Functions/passing_into_to_functions.py
@@ -4,5 +4,13 @@ def hello_world(username):
     """showing a username"""
     print(" Hello " + username.title() + ".")
 
-# We pass the argument Rene into the function 
+# We pass the argument Rene into the function
 hello_world('rene')
+
+# Positional argument
+# Creating our function
+def book_description(book_type, author_name):
+    print("\nThis Book is " + book_type + ".")
+    print("The author of this book is " + author_name.title() + ".")
+
+book_description('non-fiction', 'ashlee vance')
