2498cb0b16644c303fbaafcef1c1cfe699909a2d
Rene Cortez <rcortezk9@gmail.com>
2019-01-14 20:16:32 -0600

    Passing argument into a function

---
+++ Functions/passing_into_to_functions.py
@@ -0,0 +1,8 @@
+# Passing information to a function
+
+def hello_world(username):
+    """showing a username"""
+    print(" Hello " + username.title() + ".")
+
+# We pass the argument Rene into the function 
+hello_world('rene')
