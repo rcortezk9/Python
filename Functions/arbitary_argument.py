3fa7f78cba8d547850eee1bf5be09df17298d286
Rene Cortez <rcortezk9@gmail.com>
2019-01-16 22:01:53 -0600

    Loop through each item and print.

--- Functions/arbitary_argument.py
+++ Functions/arbitary_argument.py
@@ -2,8 +2,16 @@
 
 # In the function definition we use an asterisk (*)
 # before the parameter name to denote this kind of argument.
-def create_passenger(*request):
-    """Print user requests"""
-    print(request)
+def create_passenger(*requests):
+    """Summarize passenger request"""
+    for request in requests:
+        print("-" + request)
 
-create_passenger('window sea', 'seat near top of the plan', 'pre-order breakfast')
+create_passenger('window seat', 'seat near top of the plan', 'pre-order breakfast')
+
+
+# output :
+
+# -window seat
+# -seat near top of the plan
+# -pre-order breakfast
