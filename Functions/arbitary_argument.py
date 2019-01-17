2804f6d5dda58e8d49f8cd137586d745ef05ce33
Rene Cortez <rcortezk9@gmail.com>
2019-01-16 21:52:27 -0600

    Created a def that would take in an arbitary number of arguments.

---
+++ Functions/arbitary_argument.py
@@ -0,0 +1,7 @@
+# Arbitary arguments
+
+def create_passenger(*request):
+    """Print user requests"""
+    print(request)
+
+create_passenger('window sea', 'seat near top of the plan', 'pre-order breakfast')
