062054404a5d394204627b99749100a81bff9f1d
Rene Cortez <rcortezk9@gmail.com>
2019-01-16 20:53:29 -0600

    Used a while loop to ask for name and break to stop loop.

---
+++ Functions/while_loop_function.py
@@ -0,0 +1,20 @@
+# Using a while loop in a function
+
+def get_formatted_name(first_name, last_name):
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+while True:
+    print("\nWhat is your name? ")
+    print("(Press 'q' to any time to quit this program)")
+
+    first_name = input("First Name: ")
+    if first_name == 'q' :
+        break
+
+    last_name = input("Last Name: ")
+    if last_name == 'q' :
+        break
+
+    formatted_name = get_formatted_name(first_name, last_name)
+    print("\nHello, " + formatted_name + "!")
