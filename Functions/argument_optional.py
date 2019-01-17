94f4948783043c09b1c533bf10bd9422db912f7d
Rene Cortez <rcortezk9@gmail.com>
2019-01-16 20:32:05 -0600

    Added a contional statment to make the middle name option as an argument.

--- Functions/argument_optional.py
+++ Functions/argument_optional.py
@@ -1,8 +1,20 @@
 # Making arguments optional
 
-def formatted_name(first_name, middle_name, last_name):
-    full_name = first_name + ' ' + middle_name + ' ' + last_name
+# Since the middle name is going to be optional you want to move
+# that parameter to the end and assigning an empty string
+def formatted_name(first_name, last_name, middle_name=''):
+
+    if middle_name:
+        full_name = first_name + ' ' + middle_name + ' ' + last_name
+    else:
+        full_name = first_name + ' ' + last_name
     return full_name.title()
 
-teacher = formatted_name('rene', 'morales', 'cortez')
+# If you are to use it as an option you can enter the middle name
+# as the last argument
+teacher = formatted_name('rene', 'cortez', 'morales')
+print(teacher)
+
+# Optional middle name 
+teacher = formatted_name('rene', 'cortez')
 print(teacher)
