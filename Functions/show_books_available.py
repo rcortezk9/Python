69a6aff516dcf468f713ac3fcff3013778823e51
Rene Cortez <rcortezk9@gmail.com>
2019-01-17 21:03:52 -0600

    Created a file for only def. Imported the model into another file.

---
+++ Functions/show_books_available.py
@@ -0,0 +1,10 @@
+# Importing a module
+
+import books_to_buy
+
+books_to_buy.books_available('Elon Musk', 'The everything store')
+
+# Output
+
+# The following title is available to buy Elon Musk
+# The following title is available to buy The Everything Store
