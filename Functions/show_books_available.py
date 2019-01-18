19dabb10a48219fc3b7d626b577433c190a663fe
Rene Cortez <rcortezk9@gmail.com>
2019-01-17 21:29:05 -0600

    In comments show the how to use alias.

--- Functions/show_books_available.py
+++ Functions/show_books_available.py
@@ -1,12 +1,14 @@
 # Importing a module
 
 from books_to_buy import books_available, book_description # Import just a function
+# To use alias you would put in "as ba" short version of one of the other function
 
 print("This is our first functions: ")
 books_available('Elon Musk')
 
 print("This is our 2nd function: ")
 book_description('ashlee vance')
+# For alias this would change to ba('ashlee vance')
 
 # Output
 
