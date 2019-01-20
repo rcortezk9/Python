9a457b3716bf79edb0ac47520c8ca3770ac2ad98
Rene Cortez <rcortezk9@gmail.com>
2019-01-20 12:47:40 -0600

    Create a class call book.

---
+++ Classes/book.py
@@ -0,0 +1,22 @@
+# Creating our first class
+
+class Books():
+    """A class to create a book."""
+
+    def __init__(self, name, price, publisher):
+        """Initialize the name, price, and publisher"""
+        self.name = name
+        self.price = price
+        self.publisher = publisher
+
+    def hardback(self):
+        """Simulate a hard back book."""
+        print(self.name.title() + " is a hardback book.")
+
+    def softback(self):
+        """Simulate being a softback"""
+        print(self.name.title() + " is a softback book.")
+
+    def ebook(self):
+        """Simulate a ebook"""
+        print(self.name.title() + " is an ebook.")
