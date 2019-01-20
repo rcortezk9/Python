bcd40d3fe643b58c4a7d4dc1d98696e63cb7b6e9
Rene Cortez <rcortezk9@gmail.com>
2019-01-20 14:56:09 -0600

    Added a default value.

--- Classes/another_book.py
+++ Classes/another_book.py
@@ -1,4 +1,5 @@
 # Creating a new ereader class
+# Setting a default value for an attribute
 
 class Ereader():
     """A class to represent an ereader."""
@@ -9,12 +10,16 @@ class Ereader():
         self.backlight = backlight
         self.battery = battery
         self.screen_type = screen_type
+        self.library_count = 0
 
     def get_ereader_name(self):
         long_name = self.make + ' - ' + self.model + ' - ' + self.backlight + ' - ' + self.battery + ' - ' + self.screen_type
 
         return long_name.title()
 
-my_new_erearder = Ereader('Amazon Kindle', 'Paperwhite', 'Adjustable', 'Several month of battery life', '300 DPI')
+    def read_library_count(self):
+        print("You have " + str(self.library_count) + " books in your kindle library.")
 
+my_new_erearder = Ereader('Amazon Kindle', 'Paperwhite', 'Adjustable', 'Several month of battery life', '300 DPI')
 print(my_new_erearder.get_ereader_name())
+my_new_erearder.read_library_count()
