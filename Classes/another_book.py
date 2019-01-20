6d9948f49f82069a5b3eacce9922a11d3fe37380
Rene Cortez <rcortezk9@gmail.com>
2019-01-20 15:04:23 -0600

    Modifying an attribute value through a method.

--- Classes/another_book.py
+++ Classes/another_book.py
@@ -1,6 +1,7 @@
 # Creating a new ereader class
 # Setting a default value for an attribute
-# Modifying and attributes value directly
+# Modifying an attributes value directly
+# Modifying an attribute value through a method
 
 class Ereader():
     """A class to represent an ereader."""
@@ -21,8 +22,11 @@ class Ereader():
     def read_library_count(self):
         print("You have " + str(self.library_count) + " books in your kindle library.")
 
+    def update_library_count(self, ebook_count):
+        self.library_count = ebook_count
+
 my_new_erearder = Ereader('Amazon Kindle', 'Paperwhite', 'Adjustable', 'Several month of battery life', '300 DPI')
 print(my_new_erearder.get_ereader_name())
 
-my_new_erearder.library_count = 36
+my_new_erearder.update_library_count(48)
 my_new_erearder.read_library_count()
