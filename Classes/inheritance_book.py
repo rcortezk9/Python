952e18c5baed14a10e194f696e858eb659707ea5
Rene Cortez <rcortezk9@gmail.com>
2019-01-21 20:20:17 -0600

    Refactor to create a screen that will hold methods for the screen.

--- Classes/inheritance_book.py
+++ Classes/inheritance_book.py
@@ -25,16 +25,24 @@ class Ereader():
     def increment_library_count(self, purchased_ebooks):
         self.library_count += purchased_ebooks
 
-class KindleFire(Ereader):
 
-    def __init__(self, make, model, backlight, battery, screen_type, screen_resolution='1280 * 800'):
+class Screen():
+    """Create a class to model the screen of a kindle fire"""
+    def __init__(self, screen_resolution='1280 * 800'):
+        """Initialize the screens attributes."""
         self.screen_resolution = screen_resolution
-        super().__init__(make, model, backlight, battery, screen_type)
 
     def describe_screen(self):
         print("\nFire HD 8 feature a widescreen " + self.screen_resolution + " HD screen.")
 
+class KindleFire(Ereader):
+
+    def __init__(self, make, model, backlight, battery, screen_type,):
+
+        super().__init__(make, model, backlight, battery, screen_type)
+        self.firescreen = Screen() # Create an new instance of screen. Store that in attribute 
+
 my_kindle_fire = KindleFire('amazon', 'kindle fire', 'backlight', '12 hour battery life', 'color screen')
 print(my_kindle_fire.get_ereader_name())
 
-my_kindle_fire.describe_screen()
+my_kindle_fire.firescreen.describe_screen()
