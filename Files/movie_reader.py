7b8908f95db007bfe202fed47f8393b915201eec
Rene Cortez <rcortezk9@gmail.com>
2019-01-22 20:42:06 -0600

    Added comment on how to open a file from a different directory.

--- Files/movie_reader.py
+++ Files/movie_reader.py
@@ -3,3 +3,5 @@
 with open('movies.txt') as file_object:
     contents = file_object.read()
     print(contents.strip())
+# if needing to open a file from different directory you would
+# 'directory/file.txt'
