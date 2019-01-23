c6e837f62c7997f69ef24b9755d142a239e42cad
Rene Cortez <rcortezk9@gmail.com>
2019-01-22 20:09:04 -0600

    Import class into another file.

---
+++ Classes/my_ereader.py
@@ -0,0 +1,4 @@
+from ereader import Ereader
+
+my_new_ereader = Ereader('amazon kindle', 'paperwite', 'adjustable backligh', 'several months of battery life', '300 dpi')
+print(my_new_ereader.get_ereader_name())
