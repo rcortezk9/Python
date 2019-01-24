cb29280ccf020d1a7632789dd2c906efc23e92df
Rene Cortez <rcortezk9@gmail.com>
2019-01-23 21:21:17 -0600

    Added pop and sort methods.

---
+++ Files/working_with_a_files_contents.py
@@ -0,0 +1,23 @@
+# Working with the contents of a sale
+
+filename = 'movies_line_by_line.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.strip())
+
+print("\n----------------------------------\n")
+
+popped_movie = lines.pop()
+
+for line in lines:
+    print(line.strip())
+
+print("\n----------------------------------\n")
+
+sorted_list = lines.sort()
+
+for line in lines:
+    print(line.strip())
