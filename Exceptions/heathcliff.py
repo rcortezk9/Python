a53dee5cba63ea1916d616cc2a9df5fc33f3bccb
Rene Cortez <rcortezk9@gmail.com>
2019-01-27 19:32:58 -0600

    Put into a function

--- Exceptions/heathcliff.py
+++ Exceptions/heathcliff.py
@@ -1,14 +1,15 @@
 # Analyzing Text
+# Working with multiple files
+def word_count(filename):
+    """count the number of words in a file"""
 
-filename = 'heathcliff.txt'
-
-try:
-    with open(filename) as file_object:
-        contents = file_object.read()
-except FileNotFoundError:
-    message = "Sorry, the file " + filename + " cannot be found"
-    print(message)
-else:
-    words = contents.split()
-    number_words = len(words)
-    print('The file ' + filename + " has approximately " + str(number_words) + " words")
+    try:
+        with open(filename) as file_object:
+            contents = file_object.read()
+    except FileNotFoundError:
+        message = "Sorry, the file " + filename + " cannot be found"
+        print(message)
+    else:
+        words = contents.split()
+        number_words = len(words)
+        print('The file ' + filename + " has approximately " + str(number_words) + " words")
