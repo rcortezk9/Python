4f83e3f635e5a6e119d0fac1fc3730775143cc16
Rene Cortez <rcortezk9@gmail.com>
2019-01-27 20:28:05 -0600

    Ask user for username then store in json file and recall file for later use.

---
+++ Files/storing_reading_user_data.py
@@ -0,0 +1,10 @@
+# Storing and reading user data
+
+import json
+
+username = input("What is your username? ")
+
+filename = 'username.json'
+with open(filename, 'w') as file_object:
+    json.dump(username, file_object)
+    print("Thank you, I'll remeber when you come back.")
