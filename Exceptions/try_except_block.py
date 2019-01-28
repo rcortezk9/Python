e241eb7d21d3227c531a46b106f4055ff4af7c70
Rene Cortez <rcortezk9@gmail.com>
2019-01-27 19:02:09 -0600

    Adding the else block to the try except.

--- Exceptions/try_except_block.py
+++ Exceptions/try_except_block.py
@@ -1,8 +1,25 @@
 # Using the try-exception block
 
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print("You cannot divide by zero.")
+# try:
+#     print(5/0)
+# except ZeroDivisionError:
+#     print("You cannot divide by zero.")
+#
+# print("Please enter another number.")
 
-print("Please enter another number.")
+# The else block
+print("Please enter two numbers to be divided")
+print("Enter 'q' to quit")
+
+while True:
+    first_number = input("\nFirst Number: ")
+    if first_number == 'q':
+        break
+
+    second_number = input("Second Number: ")
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You can't divide by 0.")
+    else:
+        print(answer)
