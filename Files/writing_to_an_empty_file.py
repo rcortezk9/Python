7e9c852d221306976ec79fc90b907c742f899c58
Rene Cortez <rcortezk9@gmail.com>
2019-01-27 18:33:34 -0600

    Writing multiple lines to a file useing append.

--- Files/writing_to_an_empty_file.py
+++ Files/writing_to_an_empty_file.py
@@ -1,6 +1,16 @@
 # Writting to an empty file
 
-filename = 'programming.txt'
+# filename = 'programming.txt'
+#
+# with open(filename, 'w') as file_object:
+#     file_object.write("Hello, my name is Rene.")
 
-with open(filename, 'w') as file_object:
-    file_object.write("Hello, my name is Rene.")
+# Writing multiple lines to a file and using the append
+
+message = input("What is your favorit file?")
+print(message.title())
+
+filename = 'favorit_file.txt'
+
+with open(filename, 'a') as file_object:
+    file_object.write(message + "\n")
