6117bd5d2fd55b2a2500094e3f749fef116dae1d
Rene Cortez <rcortezk9@gmail.com>
2019-01-27 19:10:50 -0600

    Adding a FileNotFoundError

---
+++ Exceptions/missing_file.py
@@ -0,0 +1,10 @@
+# fileNotFoundError
+
+filename = 'rene.txt'
+
+try:
+    with open(filename) as file_object:
+        contents = file_object.read()
+except FileNotFoundError:
+    message = "Sorry, the file " + filename + " cannot be found"
+    print(message)
